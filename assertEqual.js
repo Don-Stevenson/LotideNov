1e2befe08ccc9a9b66ff7204d7627ef2713bc8f7
Don-Stevenson <dsteven1@gmail.com>
2019-11-25 19:58:34 +0000

    working version of assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+ if (actual === expected){
+   console.log ("✅✅✅ Assertion Passed:" + actual + "===" + expected + "✅✅✅");
+ } else {
+  console.log ("🟥🟥🟥 Assertion Failed:" + actual + "!==" + expected + "🟥🟥🟥");
+ }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
