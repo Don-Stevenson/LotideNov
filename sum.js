71bc5e17808ec1a0b432e2d28720d3afbd258d8b
Don-Stevenson <dsteven1@gmail.com>
2019-11-25 19:26:13 +0000

    working code for sum.js

---
+++ sum.js
@@ -0,0 +1,11 @@
+const args = process.argv.slice(2);
+console.log(args);
+
+const sumOfNums = (args) => {
+  let sum = 0;
+  for (let item in args) {
+    sum += parseInt(args[item]);
+  }
+  return sum;
+};
+console.log(sumOfNums(args));
