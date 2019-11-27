83d082032e26088d71a6faea5097422dc0c6c5db
Don-Stevenson <dsteven1@gmail.com>
2019-11-27 18:09:41 +0000

    working code for countOnly, not refactored

---
+++ countOnly.js
@@ -0,0 +1,44 @@
+// Function that compares two values and asserts whether they are the same or not
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected){
+    console.log (`✅✅✅ Assertion Passed: + ${actual} + === ${expected} ✅✅✅`);
+  } else {
+   console.log (`🟥🟥🟥 Assertion Failed: ${actual} !== ${expected} 🟥🟥🟥`);
+  }
+ };
+
+
+ // allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (let item of allItems) {
+   if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  return results;
+ }
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
