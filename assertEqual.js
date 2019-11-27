a3c00718e643454139f3240cdfa17f2ecec86275
Don-Stevenson <dsteven1@gmail.com>
2019-11-27 20:53:39 +0000

    working code, minus extra concats and cleaned up and linted

--- assertEqual.js
+++ assertEqual.js
@@ -2,8 +2,8 @@
 
 const assertEqual = function(actual, expected) {
  if (actual === expected){
-   console.log (`✅✅✅ Assertion Passed: + ${actual} + === ${expected} ✅✅✅`);
- } else {
+   console.log (`✅✅✅ Assertion Passed:${actual} === ${expected} ✅✅✅`);
+ } else { 
   console.log (`🟥🟥🟥 Assertion Failed: ${actual} !== ${expected} 🟥🟥🟥`);
  }
 };
