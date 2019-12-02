f8aee8f64d291e40a4d50d19ec497c93a40a6b41
Don-Stevenson <dsteven1@gmail.com>
2019-12-02 17:23:44 +0000

    refactored and mocha test working

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,14 @@
 const eqArrays = require('../eqArrays');
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse!", "Labs"]);
-eqArrays(result, ["Lighthouse!", "Labs"]); // => will always fail!
+describe("#tail", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+   eqArrays(tail([1, 2, 3]), 3);
+  });
+});
+
+
+
+
+// const result = tail(["Hello", "Lighthouse!", "Labs"]);
+// eqArrays(result, ["Lighthouse!", "Labs"]); // => will always fail!
