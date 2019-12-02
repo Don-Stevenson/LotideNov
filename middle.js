cbca2d6f6fd2da838adefe4d657666aec641d6da
Don-Stevenson <dsteven1@gmail.com>
2019-12-02 16:59:33 +0000

    refactored and working tests

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-// function that compares if two arrays are the same
-const eqArrays = (arrOne, arrTwo) => {
-  if (arrOne.length !== arrTwo.length) {
-    return false;
-  } for (let i = 0; i < arrTwo.length; i++) {
-    if (arrOne[i] !== arrTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-//Function that asserts if two arrays are equal to one another
-const assertArraysEqual = (arrOne, arrTwo) =>{
-  if (eqArrays(arrOne, arrTwo)){
-    console.log (`✅✅✅ Assertion Passed: ${arrOne} === ${arrTwo} ✅✅✅`);
-  } else {
-   console.log (`🟥🟥🟥 Assertion Failed: ${arrOne} !== ${arrTwo} 🟥🟥🟥`);
-  }
- };
-
 //function that gets the middle of the array
 
 const middle = (array) => {
@@ -33,5 +12,4 @@ const middle = (array) => {
   }
 }
 
-assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
+module.exports = middle;
